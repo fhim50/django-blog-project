8691a72c36d631f4e8441a33d10efff6ea6bc9c1
Kwaw Annor <kwaw@kwaw-HP-625.(none)>
2012-07-05 17:25:01 +0000

    new changes

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib import admin
 # Create your models here.
+
 class Post(models.Model):
     title = models.CharField(max_length=60)
     body = models.TextField()
@@ -8,6 +9,7 @@ class Post(models.Model):
     updated = models.DateField()
     def __unicode__(self):
 	return self.title
+    
 class Comment(models.Model):
     body=models.TextField()
     author= models.CharField(max_length=60)
@@ -16,5 +18,34 @@ class Comment(models.Model):
     def __unicode__(self):
 	return self.body
     post = models.ForeignKey(Post)
+
+class CommentInline(admin.TabularInline):
+    model=Comment
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','created','updated')
+    search_fields= ('title','body')
+    list_filter = ('created',)
+    inlines = [
+	CommentInline,
+    ]
+
+
+
+class CommentAdmin(admin.ModelAdmin):
+    def body_first_60(self):
+       return self.body[:60]
+    list_display=('post','author','body_first_60','created','updated')
+    list_filter = ('created','author')
+    
+  
+
+
+
+
+    
+
+#admin.site.register(Post,PostAdmin)
+#admin.site.register(Comment,CommentAdmin)
 admin.site.register(Post)
 admin.site.register(Comment)
